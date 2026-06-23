324471ef89f6efd5e731c244d5ea95936360a167
caparo-bit <carlosparronramos@gmail.com>
2026-06-23 16:54:28 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,5 @@
 const CACHE = 'tazi-v1';
-const ASSETS = [
-  '/Tazi/',
-  '/Tazi/index.html',
-  '/Tazi/manifest.json',
-  '/Tazi/icon-192.png',
-  '/Tazi/icon-512.png'   // si lo tienes
-];
+const ASSETS = ['/'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
